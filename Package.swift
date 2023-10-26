188f860c805d66d0d4d4596c4f80869c9da88c5f
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-26 12:01:49 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -8,15 +8,15 @@ let package = Package(
 		.library(name: "OPML", targets: ["OPML"])
 	],
 	dependencies: [
-        .package(url: "https://github.com/hallee/swift-html.git", branch: "main")
+        .package(url: "https://github.com/lake-of-fire/swift-html.git", branch: "main")
 	],
 	targets: [
 		.target(name: "OPML", dependencies: [
             .product(name: "Html", package: "swift-html"),
-        ]),
+        ])/*,
 		.testTarget(
 			name: "Tests",
 			dependencies: ["OPML"]
-		)
+		)*/
 	]
 )

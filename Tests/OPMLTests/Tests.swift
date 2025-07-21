1aa8208653bd4713b0f3a04859651bb962b156da
Alex Ehlke <alex.ehlke@gmail.com>
2025-07-21 16:58:37 -0400

    reenable tests

--- Tests/OPMLTests/Tests.swift
+++ Tests/OPMLTests/Tests.swift
@@ -1,44 +1,44 @@
-//import XCTest
-//@testable import OPML
-//
-//class Tests: XCTestCase {
-//
-//	func testOPMLNested() {
-//		guard let file = Bundle.module.url(forResource: "rsparser", withExtension: "opml") else {
-//			XCTFail("Missing opml file")
-//			return
-//		}
-//
-//		do {
-//			let parser = try OPMLParser(file: file)
-//			let opml = try parser.parse()
-//			XCTAssertEqual(opml.entries.flatMap { $0.children ?? [] }.count, 138)
-//
-//			let programmingEntries = opml.entries.first(where: { $0.text == "Programming" })?.children
-//			XCTAssertEqual(programmingEntries?.count ?? 0, 33)
-//		} catch {
-//			XCTFail(error.localizedDescription)
-//		}
-//
-//	}
-//
-//	func testOPML2() {
-//		guard let file = Bundle.module.url(forResource: "feedly", withExtension: "opml") else {
-//			XCTFail("Missing opml file")
-//			return
-//		}
-//
-//		do {
-//			let parser = try OPMLParser(file: file)
-//			let opml = try parser.parse()
-//			XCTAssertEqual(opml.entries.count, 39)
-//		} catch {
-//			XCTFail(error.localizedDescription)
-//		}
-//	}
-//
-//	func testMissingFile() {
-//		XCTAssertThrowsError(try OPMLParser(file: URL(string: "file:///Resources/nonExistentFile.opml")!).parse())
-//	}
-//
-//}
+import XCTest
+@testable import OPML
+
+class Tests: XCTestCase {
+
+	func testOPMLNested() {
+		guard let file = Bundle.module.url(forResource: "rsparser", withExtension: "opml") else {
+			XCTFail("Missing opml file")
+			return
+		}
+
+		do {
+			let parser = try OPMLParser(file: file)
+			let opml = try parser.parse()
+			XCTAssertEqual(opml.entries.flatMap { $0.children ?? [] }.count, 138)
+
+			let programmingEntries = opml.entries.first(where: { $0.text == "Programming" })?.children
+			XCTAssertEqual(programmingEntries?.count ?? 0, 33)
+		} catch {
+			XCTFail(error.localizedDescription)
+		}
+
+	}
+
+	func testOPML2() {
+		guard let file = Bundle.module.url(forResource: "feedly", withExtension: "opml") else {
+			XCTFail("Missing opml file")
+			return
+		}
+
+		do {
+			let parser = try OPMLParser(file: file)
+			let opml = try parser.parse()
+			XCTAssertEqual(opml.entries.count, 39)
+		} catch {
+			XCTFail(error.localizedDescription)
+		}
+	}
+
+	func testMissingFile() {
+		XCTAssertThrowsError(try OPMLParser(file: URL(string: "file:///Resources/nonExistentFile.opml")!).parse())
+	}
+
+}
